48b55bcf27c50faf8cd7d5a272a2915cb4293efc
Alek Petuskey <17petuskey@berkeley.edu>
2023-04-30 20:15:45 -0700

    Delete llm_web_ui directory

--- llm_web_ui/llm_web_ui/styles.py
+++
@@ -1,8 +0,0 @@
-accent_color = "rgba(247,247,247, 1)"
-border_color = "rgba(234,234,234, 1)"
-
-green_color = "rgba(96, 203, 150, 1)"
-light_green_color = "rgba(96, 203, 150, .25)"
-
-text_dark_color = "rgba(54, 65, 83, 1)"
-text_accent_color = "rgba(103,116,137, 1)"
