a94f75fa42bc6f33873983a194a0d0e8220b1184
Aleksander Petuskey <17petuskey@berkeley.edu>
2023-04-30 20:15:01 -0700

    refactor

---
+++ webui/pcconfig.py
@@ -0,0 +1,13 @@
+import pynecone as pc
+
+class WebuiConfig(pc.Config):
+    pass
+
+config = WebuiConfig(
+    app_name="webui",
+    db_url="sqlite:///pynecone.db",
+    env=pc.Env.DEV,
+    frontend_packages=[
+        "react-loading-icons",
+    ]
+)
