3db87a1a0152259625b056903befc8b65c60b156
jiangyangcreate <jiangyangcreate@gmail.com>
2023-12-19 12:41:24 +0800

    Update chat.py

--- webui/webui/components/chat.py
+++ webui/webui/components/chat.py
@@ -1,107 +1,208 @@
+import os
+import requests
+import json
+import openai
 import reflex as rx
 
-from webui import styles
-from webui.components import loading_icon
-from webui.state import QA, State
+openai.api_key = os.environ["OPENAI_API_KEY"]
+openai.api_base = os.getenv("OPENAI_API_BASE", "https://api.openai.com/v1")
+
+BAIDU_API_KEY = os.environ["BAIDU_API_KEY"]
+BAIDU_SECRET_KEY = os.environ["BAIDU_SECRET_KEY"]
 
 
-def message(qa: QA) -> rx.Component:
-    """A single question/answer message.
-
-    Args:
-        qa: The question/answer pair.
-
-    Returns:
-        A component displaying the question/answer pair.
+def get_access_token():
     """
-    return rx.box(
-        rx.box(
-            rx.text(
-                qa.question,
-                bg=styles.border_color,
-                shadow=styles.shadow_light,
-                **styles.message_style,
-            ),
-            text_align="right",
-            margin_top="1em",
-        ),
-        rx.box(
-            rx.text(
-                qa.answer,
-                bg=styles.accent_color,
-                shadow=styles.shadow_light,
-                **styles.message_style,
-            ),
-            text_align="left",
-            padding_top="1em",
-        ),
-        width="100%",
-    )
+    :return: access_token
+    """
+    url = "https://aip.baidubce.com/oauth/2.0/token"
+    params = {
+        "grant_type": "client_credentials",
+        "client_id": BAIDU_API_KEY,
+        "client_secret": BAIDU_SECRET_KEY,
+    }
+    return str(requests.post(url, params=params).json().get("access_token"))
 
 
-def chat() -> rx.Component:
-    """List all the messages in a single conversation."""
-    return rx.vstack(
-        rx.box(rx.foreach(State.chats[State.current_chat], message)),
-        py="8",
-        flex="1",
-        width="100%",
-        max_w="3xl",
-        padding_x="4",
-        align_self="center",
-        overflow="hidden",
-        padding_bottom="5em",
-    )
+class QA(rx.Base):
+    """A question and answer pair."""
+
+    question: str
+    answer: str
 
 
-def action_bar() -> rx.Component:
-    """The action bar to send a new message."""
-    return rx.box(
-        rx.vstack(
-            rx.form(
-                rx.form_control(
-                    rx.hstack(
-                        rx.input(
-                            placeholder="Type something...",
-                            id="question",
-                            on_change=State.set_question,
-                            _placeholder={"color": "#fffa"},
-                            _hover={"border_color": styles.accent_color},
-                            style=styles.input_style,
-                        ),
-                        rx.button(
-                            rx.cond(
-                                State.processing,
-                                loading_icon(height="1em"),
-                                rx.text("Send"),
-                            ),
-                            type_="submit",
-                            _hover={"bg": styles.accent_color},
-                            style=styles.input_style,
-                        ),
-                    ),
-                    is_disabled=State.processing,
-                ),
-                on_submit=[State.process_question, rx.set_value("question", "")],
-                width="100%",
-            ),
-            rx.text(
-                "ReflexGPT may return factually incorrect or misleading responses. Use discretion.",
-                font_size="xs",
-                color="#fff6",
-                text_align="center",
-            ),
-            width="100%",
-            max_w="3xl",
-            mx="auto",
-        ),
-        position="sticky",
-        bottom="0",
-        left="0",
-        py="4",
-        backdrop_filter="auto",
-        backdrop_blur="lg",
-        border_top=f"1px solid {styles.border_color}",
-        align_items="stretch",
-        width="100%",
-    )
+DEFAULT_CHATS = {
+    "Intros": [],
+}
+
+
+class State(rx.State):
+    """The app state."""
+
+    # A dict from the chat name to the list of questions and answers.
+    chats: dict[str, list[QA]] = DEFAULT_CHATS
+
+    # The current chat name.
+    current_chat = "Intros"
+
+    # The current question.
+    question: str
+
+    # Whether we are processing the question.
+    processing: bool = False
+
+    # The name of the new chat.
+    new_chat_name: str = ""
+
+    # Whether the drawer is open.
+    drawer_open: bool = False
+
+    # Whether the modal is open.
+    modal_open: bool = False
+
+    api_type: str = "baidu"
+
+    def create_chat(self):
+        """Create a new chat."""
+        # Add the new chat to the list of chats.
+        self.current_chat = self.new_chat_name
+        self.chats[self.new_chat_name] = []
+
+        # Toggle the modal.
+        self.modal_open = False
+
+    def toggle_modal(self):
+        """Toggle the new chat modal."""
+        self.modal_open = not self.modal_open
+
+    def toggle_drawer(self):
+        """Toggle the drawer."""
+        self.drawer_open = not self.drawer_open
+
+    def delete_chat(self):
+        """Delete the current chat."""
+        del self.chats[self.current_chat]
+        if len(self.chats) == 0:
+            self.chats = DEFAULT_CHATS
+        self.current_chat = list(self.chats.keys())[0]
+        self.toggle_drawer()
+
+    def set_chat(self, chat_name: str):
+        """Set the name of the current chat.
+
+        Args:
+            chat_name: The name of the chat.
+        """
+        self.current_chat = chat_name
+        self.toggle_drawer()
+
+    @rx.var
+    def chat_titles(self) -> list[str]:
+        """Get the list of chat titles.
+
+        Returns:
+            The list of chat names.
+        """
+        return list(self.chats.keys())
+
+    async def process_question(self, form_data: dict[str, str]):
+        if self.api_type == "openai":
+            model = self.openai_process_question
+        else:
+            model = self.baidu_process_question
+
+        async for value in model(form_data):
+            yield value
+
+    async def openai_process_question(self, form_data: dict[str, str]):
+        """Get the response from the API.
+
+        Args:
+            form_data: A dict with the current question.
+        """
+        # Check if the question is empty
+        if self.question == "":
+            return
+
+        # Add the question to the list of questions.
+        qa = QA(question=self.question, answer="")
+        self.chats[self.current_chat].append(qa)
+
+        # Clear the input and start the processing.
+        self.processing = True
+        self.question = ""
+        yield
+
+        # Build the messages.
+        messages = [
+            {"role": "system", "content": "You are a friendly chatbot named Reflex."}
+        ]
+        for qa in self.chats[self.current_chat]:
+            messages.append({"role": "user", "content": qa.question})
+            messages.append({"role": "assistant", "content": qa.answer})
+
+        # Remove the last mock answer.
+        messages = messages[:-1]
+
+        # Start a new session to answer the question.
+        session = openai.ChatCompletion.create(
+            model=os.getenv("OPENAI_MODEL", "gpt-3.5-turbo"),
+            messages=messages,
+            stream=True,
+        )
+
+        # Stream the results, yielding after every word.
+        for item in session:
+            if hasattr(item.choices[0].delta, "content"):
+                answer_text = item.choices[0].delta.content
+                self.chats[self.current_chat][-1].answer += answer_text
+                self.chats = self.chats
+                yield
+
+        # Toggle the processing flag.
+        self.processing = False
+
+    async def baidu_process_question(self, form_data: dict[str, str]):
+        """Get the response from the API.
+
+        Args:
+            form_data: A dict with the current question.
+        """
+        # Check if the question is empty
+        if self.question == "":
+            return
+
+        # Add the question to the list of questions.
+        qa = QA(question=self.question, answer="")
+        self.chats[self.current_chat].append(qa)
+
+        # Clear the input and start the processing.
+        self.processing = True
+        self.question = ""
+        yield
+
+        # Build the messages.
+        messages = []
+        for qa in self.chats[self.current_chat]:
+            messages.append({"role": "user", "content": qa.question})
+            messages.append({"role": "assistant", "content": qa.answer})
+
+        # Remove the last mock answer.
+        messages = json.dumps({"messages": messages[:-1]})
+        # Start a new session to answer the question.
+        session = requests.request(
+            "POST",
+            "https://aip.baidubce.com/rpc/2.0/ai_custom/v1/wenxinworkshop/chat/completions_pro?access_token="
+            + get_access_token(),
+            headers={"Content-Type": "application/json"},
+            data=messages,
+        )
+
+        json_data = json.loads(session.text)
+        if "result" in json_data.keys():
+            answer_text = json_data["result"]
+            self.chats[self.current_chat][-1].answer += answer_text
+            self.chats = self.chats
+            yield
+        # Toggle the processing flag.
+        self.processing = False
