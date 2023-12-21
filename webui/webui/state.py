de9952ce930771799daf5551ec47711b487bc868
jiangyangcreate <jiangyangcreate@gmail.com>
2023-12-21 10:13:06 +0800

    add baidu api

--- webui/webui/state.py
+++ webui/webui/state.py
@@ -3,8 +3,27 @@ import os
 import openai
 import reflex as rx
 
+import requests
+import json
+
 openai.api_key = os.environ["OPENAI_API_KEY"]
 openai.api_base = os.getenv("OPENAI_API_BASE", "https://api.openai.com/v1")
+
+BAIDU_API_KEY = os.environ["BAIDU_API_KEY"]
+BAIDU_SECRET_KEY = os.environ["BAIDU_SECRET_KEY"]
+
+
+def get_access_token():
+    """
+    :return: access_token
+    """
+    url = "https://aip.baidubce.com/oauth/2.0/token"
+    params = {
+        "grant_type": "client_credentials",
+        "client_id": BAIDU_API_KEY,
+        "client_secret": BAIDU_SECRET_KEY,
+    }
+    return str(requests.post(url, params=params).json().get("access_token"))
 
 
 class QA(rx.Base):
@@ -42,6 +61,9 @@ class State(rx.State):
 
     # Whether the modal is open.
     modal_open: bool = False
+
+    # Whether the api_type is baidu.
+    api_type: str = "baidu"
 
     def create_chat(self):
         """Create a new chat."""
@@ -87,6 +109,15 @@ class State(rx.State):
         return list(self.chats.keys())
 
     async def process_question(self, form_data: dict[str, str]):
+        if self.api_type == "openai":
+            model = self.openai_process_question
+        else:
+            model = self.baidu_process_question
+
+        async for value in model(form_data):
+            yield value
+
+    async def openai_process_question(self, form_data: dict[str, str]):
         """Get the response from the API.
 
         Args:
@@ -131,5 +162,49 @@ class State(rx.State):
                 self.chats = self.chats
                 yield
 
+        # Toggle the processing flag.
+        self.processing = False
+
+    async def baidu_process_question(self, form_data: dict[str, str]):
+        """Get the response from the API.
+        Args:
+            form_data: A dict with the current question.
+        """
+        # Check if the question is empty
+        if self.question == "":
+            return
+
+        # Add the question to the list of questions.
+        qa = QA(question=self.question, answer="")
+        self.chats[self.current_chat].append(qa)
+
+        # Clear the input and start the processing.
+        self.processing = True
+        self.question = ""
+        yield
+
+        # Build the messages.
+        messages = []
+        for qa in self.chats[self.current_chat]:
+            messages.append({"role": "user", "content": qa.question})
+            messages.append({"role": "assistant", "content": qa.answer})
+
+        # Remove the last mock answer.
+        messages = json.dumps({"messages": messages[:-1]})
+        # Start a new session to answer the question.
+        session = requests.request(
+            "POST",
+            "https://aip.baidubce.com/rpc/2.0/ai_custom/v1/wenxinworkshop/chat/completions_pro?access_token="
+            + get_access_token(),
+            headers={"Content-Type": "application/json"},
+            data=messages,
+        )
+
+        json_data = json.loads(session.text)
+        if "result" in json_data.keys():
+            answer_text = json_data["result"]
+            self.chats[self.current_chat][-1].answer += answer_text
+            self.chats = self.chats
+            yield
         # Toggle the processing flag.
         self.processing = False
