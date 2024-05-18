da99c571c2a53b6bbe9efff79ef2db09cd38f2fd
Sagar Hedaoo <sagarhedaoo@gmail.com>
2024-05-18 01:28:47 -0400

    Added the UI for like/dislike

--- chat/state.py
+++ chat/state.py
@@ -2,7 +2,6 @@ import os
 import reflex as rx
 from openai import OpenAI
 
-
 # Checking if the API key is set properly
 if not os.getenv("OPENAI_API_KEY"):
     raise Exception("Please set OPENAI_API_KEY environment variable.")
@@ -13,6 +12,8 @@ class QA(rx.Base):
 
     question: str
     answer: str
+    likes: int = 0
+    dislikes: int = 0
 
 
 DEFAULT_CHATS = {
@@ -52,20 +53,12 @@ class State(rx.State):
         self.current_chat = list(self.chats.keys())[0]
 
     def set_chat(self, chat_name: str):
-        """Set the name of the current chat.
-
-        Args:
-            chat_name: The name of the chat.
-        """
+        """Set the name of the current chat."""
         self.current_chat = chat_name
 
     @rx.var
     def chat_titles(self) -> list[str]:
-        """Get the list of chat titles.
-
-        Returns:
-            The list of chat names.
-        """
+        """Get the list of chat titles."""
         return list(self.chats.keys())
 
     async def process_question(self, form_data: dict[str, str]):
@@ -82,21 +75,14 @@ class State(rx.State):
             yield value
 
     async def openai_process_question(self, question: str):
-        """Get the response from the API.
-
-        Args:
-            form_data: A dict with the current question.
-        """
-
-        # Add the question to the list of questions.
-        qa = QA(question=question, answer="")
+        """Get the response from the API."""
+        qa = QA(question=question, answer="", likes=0, dislikes=0)
         self.chats[self.current_chat].append(qa)
 
         # Clear the input and start the processing.
         self.processing = True
         yield
 
-        # Build the messages.
         messages = [
             {
                 "role": "system",
@@ -121,12 +107,9 @@ class State(rx.State):
         for item in session:
             if hasattr(item.choices[0].delta, "content"):
                 answer_text = item.choices[0].delta.content
-                # Ensure answer_text is not None before concatenation
                 if answer_text is not None:
                     self.chats[self.current_chat][-1].answer += answer_text
                 else:
-                    # Handle the case where answer_text is None, perhaps log it or assign a default value
-                    # For example, assigning an empty string if answer_text is None
                     answer_text = ""
                     self.chats[self.current_chat][-1].answer += answer_text
                 self.chats = self.chats
